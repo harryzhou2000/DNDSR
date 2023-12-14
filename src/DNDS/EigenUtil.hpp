10fe0216a2ee3745acec4d8251ad39717bfa823e
HarryZhou <2373256746@qq.com>
2023-12-14 10:23:07 +0800

    to_string for Eigen::DenseBase

---
+++ src/DNDS/EigenUtil.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Defines.hpp"
+
+#include <fmt/core.h>
+#include <Eigen/Core>
+#include <fmt/ostream.h>
+
+namespace DNDS
+{
+    //TODO: lessen copying chance?
+    template <class dir>
+    constexpr std::string to_string(const Eigen::DenseBase<dir> &v,
+                          int precision = 5,
+                          bool scientific = false)
+    {
+        std::stringstream ss;
+        if (precision > 0)
+            ss << std::setprecision(precision);
+        if (scientific)
+            ss << std::scientific;
+        ss << v;
+        return ss.str();
+    }
+}
