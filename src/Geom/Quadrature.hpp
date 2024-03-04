d154f613e931c09067a79827b0f529d4947b6799
HarryZhou <2373256746@qq.com>
2024-03-04 20:48:01 +0800

    added wallDistScheme=1 for better curve-mesh dWall

--- src/Geom/Quadrature.hpp
+++ src/Geom/Quadrature.hpp
@@ -739,4 +739,294 @@ namespace DNDS::Geom::Elem
 
         t_index GetNumPoints() { return int_scheme; }
     };
+
+    inline auto GetQuadPatches(Quadrature &q)
+    {
+        std::vector<std::array<t_index, 3>> ret;
+        using tArr = std::array<t_index, 3>;
+        ret.reserve(40);
+        if (q.elem.GetParamSpace() == ParamSpace::LineSpace)
+        {
+            if (q.elem.GetOrder() == 1 || q.elem.GetOrder() == 2) // omitting the middle point
+            {
+                ret.emplace_back(tArr{1, -1, 0});
+                for (int i = 1; i < q.GetNumPoints(); i++)
+                    ret.emplace_back(tArr{-i, -1 - i, 0});
+                ret.emplace_back(tArr{-q.GetNumPoints(), 2, 0});
+            }
+        }
+        else if (q.elem.GetParamSpace() == ParamSpace::QuadSpace)
+        {
+            if (q.elem.GetOrder() == 1 || q.elem.GetOrder() == 2) // omitting the middle point
+            {
+                t_index nGL = std::round(std::sqrt(q.GetNumPoints()));
+                auto ijG = [=](int i, int j)
+                { return nGL * j + i; };
+                t_index iMax = nGL - 1;
+
+                if (q.elem.GetOrder() == 1)
+                {
+                    ret.emplace_back(tArr{1, 2, -1 - ijG(0, 0)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(i + 1, 0), -1 - ijG(i, 0), 2});
+                    ret.emplace_back(tArr{3, 4, -1 - ijG(iMax, iMax)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(i, iMax), -1 - ijG(i + 1, iMax), 4});
+                    ret.emplace_back(tArr{2, 3, -1 - ijG(iMax, 0)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(iMax, i + 1), -1 - ijG(iMax, i), 3});
+                    ret.emplace_back(tArr{4, 1, -1 - ijG(0, iMax)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(0, i), -1 - ijG(0, i + 1), 1});
+                }
+                else if (q.elem.GetOrder() == 2)
+                {
+                    ret.emplace_back(tArr{1, 5, -1 - ijG(0, 0)});
+                    ret.emplace_back(tArr{5, 2, -1 - ijG(iMax, 0)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(i + 1, 0), -1 - ijG(i, 0), 5});
+                    ret.emplace_back(tArr{3, 7, -1 - ijG(iMax, iMax)});
+                    ret.emplace_back(tArr{7, 4, -1 - ijG(0, iMax)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(i, iMax), -1 - ijG(i + 1, iMax), 7});
+                    ret.emplace_back(tArr{2, 6, -1 - ijG(iMax, 0)});
+                    ret.emplace_back(tArr{6, 3, -1 - ijG(iMax, iMax)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(iMax, i + 1), -1 - ijG(iMax, i), 6});
+                    ret.emplace_back(tArr{4, 8, -1 - ijG(0, iMax)});
+                    ret.emplace_back(tArr{8, 1, -1 - ijG(0, 0)});
+                    for (int i = 0; i < iMax; i++)
+                        ret.emplace_back(tArr{-1 - ijG(0, i), -1 - ijG(0, i + 1), 8});
+                }
+
+                for (int i = 0; i < iMax; i++)
+                    for (int j = 0; j < iMax; j++)
+                    {
+                        ret.emplace_back(tArr{-1 - ijG(i, j), -1 - ijG(i + 1, j), -1 - ijG(i, j + 1)});
+                        ret.emplace_back(tArr{-1 - ijG(i + 1, j + 1), -1 - ijG(i, j + 1), -1 - ijG(i + 1, j)});
+                    }
+            }
+        }
+        else if (q.elem.GetParamSpace() == ParamSpace::TriSpace)
+        {
+            if (q.GetNumPoints() == 1)
+            {
+                if (q.elem.GetOrder() == 1)
+                {
+                    ret.emplace_back(tArr{1, 2, -1});
+                    ret.emplace_back(tArr{2, 3, -1});
+                    ret.emplace_back(tArr{3, 1, -1});
+                }
+                else if (q.elem.GetOrder() == 2)
+                {
+                    ret.emplace_back(tArr{1, 4, -1});
+                    ret.emplace_back(tArr{4, 2, -1});
+                    ret.emplace_back(tArr{2, 5, -1});
+                    ret.emplace_back(tArr{5, 3, -1});
+                    ret.emplace_back(tArr{3, 6, -1});
+                    ret.emplace_back(tArr{6, 1, -1});
+                }
+            }
+            else if (q.GetNumPoints() == 3)
+            {
+                if (q.elem.GetOrder() == 1)
+                {
+                    ret.emplace_back(tArr{1, 2, -3});
+                    ret.emplace_back(tArr{2, -1, -3});
+                    ret.emplace_back(tArr{2, 3, -1});
+                    ret.emplace_back(tArr{3, -2, -1});
+                    ret.emplace_back(tArr{3, 1, -2});
+                    ret.emplace_back(tArr{1, -3, -2});
+
+                    ret.emplace_back(tArr{-1, -2, -3});
+                }
+                else if (q.elem.GetOrder() == 2)
+                {
+                    ret.emplace_back(tArr{1, 4, -3});
+                    ret.emplace_back(tArr{4, 2, -1});
+                    ret.emplace_back(tArr{2, 5, -1});
+                    ret.emplace_back(tArr{5, 3, -2});
+                    ret.emplace_back(tArr{3, 6, -2});
+                    ret.emplace_back(tArr{6, 1, -3});
+
+                    ret.emplace_back(tArr{-1, -3, 4});
+                    ret.emplace_back(tArr{-2, -1, 5});
+                    ret.emplace_back(tArr{-3, -2, 6});
+
+                    ret.emplace_back(tArr{-1, -2, -3});
+                }
+            }
+            else if (q.GetNumPoints() == 6)
+            {
+                if (q.elem.GetOrder() == 1)
+                {
+                    ret.emplace_back(tArr{1, 2, -6});
+                    ret.emplace_back(tArr{2, 3, -4});
+                    ret.emplace_back(tArr{3, 1, -5});
+
+                    ret.emplace_back(tArr{-6, -1, 1});
+                    ret.emplace_back(tArr{-2, -6, 2});
+                    ret.emplace_back(tArr{-4, -2, 2});
+                    ret.emplace_back(tArr{-3, -4, 3});
+                    ret.emplace_back(tArr{-5, -3, 3});
+                    ret.emplace_back(tArr{-1, -5, 1});
+
+                    ret.emplace_back(tArr{-4, -5, -6});
+                    ret.emplace_back(tArr{-1, -6, -5});
+                    ret.emplace_back(tArr{-6, -2, -4});
+                    ret.emplace_back(tArr{-5, -4, -3});
+                }
+                else if (q.elem.GetOrder() == 2)
+                {
+                    ret.emplace_back(tArr{1, 4, -6});
+                    ret.emplace_back(tArr{4, 2, -6});
+                    ret.emplace_back(tArr{2, 5, -4});
+                    ret.emplace_back(tArr{5, 3, -4});
+                    ret.emplace_back(tArr{3, 6, -5});
+                    ret.emplace_back(tArr{6, 1, -5});
+
+                    ret.emplace_back(tArr{-6, -1, 1});
+                    ret.emplace_back(tArr{-2, -6, 2});
+                    ret.emplace_back(tArr{-4, -2, 2});
+                    ret.emplace_back(tArr{-3, -4, 3});
+                    ret.emplace_back(tArr{-5, -3, 3});
+                    ret.emplace_back(tArr{-1, -5, 1});
+
+                    ret.emplace_back(tArr{-4, -5, -6});
+                    ret.emplace_back(tArr{-1, -6, -5});
+                    ret.emplace_back(tArr{-6, -2, -4});
+                    ret.emplace_back(tArr{-5, -4, -3});
+                }
+            }
+            else if (q.GetNumPoints() == 7)
+            {
+                static const std::array<int, 7> idxTri6ToTri7{
+                    -1, 5, 6, 7, 2, 3, 4};
+                if (q.elem.GetOrder() == 1)
+                {
+                    ret.emplace_back(tArr{1, 2, -idxTri6ToTri7[6]});
+                    ret.emplace_back(tArr{2, 3, -idxTri6ToTri7[4]});
+                    ret.emplace_back(tArr{3, 1, -idxTri6ToTri7[5]});
+
+                    ret.emplace_back(tArr{-idxTri6ToTri7[6], -idxTri6ToTri7[1], 1});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[2], -idxTri6ToTri7[6], 2});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[4], -idxTri6ToTri7[2], 2});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[3], -idxTri6ToTri7[4], 3});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[5], -idxTri6ToTri7[3], 3});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[1], -idxTri6ToTri7[5], 1});
+
+                    ret.emplace_back(tArr{-idxTri6ToTri7[4], -idxTri6ToTri7[5], -1});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[5], -idxTri6ToTri7[6], -1});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[6], -idxTri6ToTri7[4], -1});
+
+                    ret.emplace_back(tArr{-idxTri6ToTri7[1], -idxTri6ToTri7[6], -idxTri6ToTri7[5]});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[6], -idxTri6ToTri7[2], -idxTri6ToTri7[4]});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[5], -idxTri6ToTri7[4], -idxTri6ToTri7[3]});
+                }
+                else if (q.elem.GetOrder() == 2)
+                {
+                    ret.emplace_back(tArr{1, 4, -idxTri6ToTri7[6]});
+                    ret.emplace_back(tArr{4, 2, -idxTri6ToTri7[6]});
+                    ret.emplace_back(tArr{2, 5, -idxTri6ToTri7[4]});
+                    ret.emplace_back(tArr{5, 3, -idxTri6ToTri7[4]});
+                    ret.emplace_back(tArr{3, 6, -idxTri6ToTri7[5]});
+                    ret.emplace_back(tArr{6, 1, -idxTri6ToTri7[5]});
+
+                    ret.emplace_back(tArr{-idxTri6ToTri7[6], -idxTri6ToTri7[1], 1});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[2], -idxTri6ToTri7[6], 2});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[4], -idxTri6ToTri7[2], 2});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[3], -idxTri6ToTri7[4], 3});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[5], -idxTri6ToTri7[3], 3});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[1], -idxTri6ToTri7[5], 1});
+
+                    ret.emplace_back(tArr{-idxTri6ToTri7[4], -idxTri6ToTri7[5], -1});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[5], -idxTri6ToTri7[6], -1});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[6], -idxTri6ToTri7[4], -1});
+
+                    ret.emplace_back(tArr{-idxTri6ToTri7[1], -idxTri6ToTri7[6], -idxTri6ToTri7[5]});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[6], -idxTri6ToTri7[2], -idxTri6ToTri7[4]});
+                    ret.emplace_back(tArr{-idxTri6ToTri7[5], -idxTri6ToTri7[4], -idxTri6ToTri7[3]});
+                }
+            }
+            else if (q.GetNumPoints() == 12)
+            {
+                if (q.elem.GetOrder() == 1)
+                {
+                    ret.emplace_back(tArr{1, 2, -4});
+                    ret.emplace_back(tArr{2, 3, -5});
+                    ret.emplace_back(tArr{3, 1, -6});
+
+                    ret.emplace_back(tArr{-7, -4, 2});
+                    ret.emplace_back(tArr{-9, -7, 2});
+                    ret.emplace_back(tArr{-5, -9, 2});
+
+                    ret.emplace_back(tArr{-11, -5, 3});
+                    ret.emplace_back(tArr{-12, -11, 3});
+                    ret.emplace_back(tArr{-6, -12, 3});
+
+                    ret.emplace_back(tArr{-10, -6, 1});
+                    ret.emplace_back(tArr{-8, -10, 1});
+                    ret.emplace_back(tArr{-4, -8, 1});
+
+                    ret.emplace_back(tArr{-4, -7, -8});
+                    ret.emplace_back(tArr{-9, -5, -11});
+                    ret.emplace_back(tArr{-10, -12, -6});
+                    ret.emplace_back(tArr{-8, -7, -1});
+                    ret.emplace_back(tArr{-2, -9, -11});
+                    ret.emplace_back(tArr{-3, -12, -10});
+
+                    ret.emplace_back(tArr{-7, -9, -2});
+                    ret.emplace_back(tArr{-7, -2, -1});
+                    ret.emplace_back(tArr{-11, -12, -3});
+                    ret.emplace_back(tArr{-11, -3, -2});
+                    ret.emplace_back(tArr{-10, -8, -1});
+                    ret.emplace_back(tArr{-10, -1, -3});
+
+                    ret.emplace_back(tArr{-1, -2, -3});
+                }
+                else if (q.elem.GetOrder() == 2)
+                {
+                    ret.emplace_back(tArr{1, 4, -4});
+                    ret.emplace_back(tArr{4, 2, -5});
+                    ret.emplace_back(tArr{2, 5, -5});
+                    ret.emplace_back(tArr{5, 3, -6});
+                    ret.emplace_back(tArr{3, 6, -6});
+                    ret.emplace_back(tArr{6, 1, -4});
+
+                    ret.emplace_back(tArr{-7, -4, 4});
+                    ret.emplace_back(tArr{-9, -7, 4});
+                    ret.emplace_back(tArr{-5, -9, 4});
+
+                    ret.emplace_back(tArr{-11, -5, 5});
+                    ret.emplace_back(tArr{-12, -11, 5});
+                    ret.emplace_back(tArr{-6, -12, 5});
+
+                    ret.emplace_back(tArr{-10, -6, 6});
+                    ret.emplace_back(tArr{-8, -10, 6});
+                    ret.emplace_back(tArr{-4, -8, 6});
+
+                    ret.emplace_back(tArr{-4, -7, -8});
+                    ret.emplace_back(tArr{-9, -5, -11});
+                    ret.emplace_back(tArr{-10, -12, -6});
+                    ret.emplace_back(tArr{-8, -7, -1});
+                    ret.emplace_back(tArr{-2, -9, -11});
+                    ret.emplace_back(tArr{-3, -12, -10});
+
+                    ret.emplace_back(tArr{-7, -9, -2});
+                    ret.emplace_back(tArr{-7, -2, -1});
+                    ret.emplace_back(tArr{-11, -12, -3});
+                    ret.emplace_back(tArr{-11, -3, -2});
+                    ret.emplace_back(tArr{-10, -8, -1});
+                    ret.emplace_back(tArr{-10, -1, -3});
+
+                    ret.emplace_back(tArr{-1, -2, -3});
+                }
+            }
+            else
+                DNDS_assert(false);
+        }
+        else
+            DNDS_assert(false);
+        return ret;
+    }
 }
