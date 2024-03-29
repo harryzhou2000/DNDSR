584ec7236070a8880efb8973c06c3328fefa7fc8
HarryZhou <2373256746@qq.com>
2024-03-29 21:16:05 +0800

    now using argparse for config file finding

--- src/Euler/SingleBlockApp.hpp
+++ src/Euler/SingleBlockApp.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <argparse.hpp>
+
 #include "EulerSolver.hpp"
-
-
 
 namespace DNDS::Euler
 {
@@ -27,35 +27,63 @@ namespace DNDS::Euler
     }
 
     template <EulerModel model>
-    void RunSingleBlockConsoleApp(int argc, char *argv[])
+    int RunSingleBlockConsoleApp(int argc, char *argv[])
     {
         using namespace std::literals;
         std::string defaultConfJson = "../cases/"s + getSingleBlockAppName(model) + "_default_config.json"s;
         std::string confJson = "../cases/"s + getSingleBlockAppName(model) + "_config.json";
-        if (argc > 1 && std::stoi(argv[1]) == 1)
+
+        argparse::ArgumentParser mainParser(getSingleBlockAppName(model) + " commit "s + DNDS_MACRO_TO_STRING(DNDS_CURRENT_COMMIT_HASH));
+        std::string read_configPath;
+        mainParser.add_argument("config").default_value("");
+
+        try
         {
-            defaultConfJson = "./"s + getSingleBlockAppName(model) + "_default_config.json";
-            confJson = "./"s + getSingleBlockAppName(model) + "_config.json";
+            mainParser.parse_args(argc, argv);
+            read_configPath = mainParser.get("config");
+            if (!read_configPath.empty())
+            {
+                confJson = read_configPath;
+                std::filesystem::path p(read_configPath);
+                if (p.is_relative())
+                    p = "." / p; // so that not using a wrong path if using a file in cwd
+                defaultConfJson = getStringForcePath(p.parent_path()) + "/"s + getSingleBlockAppName(model) + "_default_config.json"s;
+            }
         }
+        catch (const std::exception &err)
+        {
+            std::cerr << err.what() << std::endl;
+            std::cerr << mainParser;
+            return 1;
+        }
+
         try
         {
             MPIInfo mpi;
             mpi.setWorld();
             auto strategy = MPI::CommStrategy::Instance().GetArrayStrategy();
             Euler::EulerSolver<model> solver(mpi);
+            if (mpi.rank == 0)
+            {
+                log() << "Reading configuration from " << confJson << std::endl;
+                log() << "Using default configuration from " << defaultConfJson << std::endl;
+            }
             solver.ConfigureFromJson(defaultConfJson, false);
             solver.ConfigureFromJson(defaultConfJson, true, confJson);
             solver.ReadMeshAndInitialize();
             solver.RunImplicitEuler();
         }
-        catch (std::exception &e)
+        catch (const std::exception &e)
         {
             std::cerr << "DNDS top-level exception: " << e.what() << std::endl;
+            return 1;
         }
         catch (...)
         {
             std::cerr << "Unknown exception" << std::endl;
+            return 1;
         }
+        return 0;
     }
 
 }
