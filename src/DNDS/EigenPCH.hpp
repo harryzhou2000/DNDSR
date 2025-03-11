ad1ce4d9841a8f5152e4535618e000bd2f53a342
HarryZhou <2373256746@qq.com>
2025-03-11 13:50:50 +0800

    added DISABLE_WARNING_CLASS_MEMACCESS for Eigen on aarch64 gcc

--- src/DNDS/EigenPCH.hpp
+++ src/DNDS/EigenPCH.hpp
@@ -3,6 +3,7 @@
 #include "Warnings.hpp"
 DISABLE_WARNING_PUSH
 DISABLE_WARNING_MAYBE_UNINITIALIZED
+DISABLE_WARNING_CLASS_MEMACCESS
 #include <Eigen/Core>
 #include <Eigen/Dense> //?It seems Mat.determinant() would be undefined rather than undeclared...
 #include <Eigen/Sparse>
--- src/DNDS/Warnings.hpp
+++ src/DNDS/Warnings.hpp
@@ -13,6 +13,7 @@
 #define DISABLE_WARNING_DEPRECATED_DECLARATIONS
 #define DISABLE_WARNING_UNUSED_VALUE
 #define DISABLE_WARNING_MAYBE_UNINITIALIZED
+#define DISABLE_WARNING_CLASS_MEMACCESS
 // other warnings you want to deactivate...
 
 #elif defined(_MSC_VER) && defined(_WIN32) && defined(__clang__) // for clang-msvc on win, change a bit from unix version
@@ -27,6 +28,7 @@
 #define DISABLE_WARNING_DEPRECATED_DECLARATIONS DISABLE_WARNING("-Wdeprecated-declarations")
 #define DISABLE_WARNING_UNUSED_VALUE DISABLE_WARNING("-Wunused-value")
 #define DISABLE_WARNING_MAYBE_UNINITIALIZED
+#define DISABLE_WARNING_CLASS_MEMACCESS DISABLE_WARNING("-Wclass-memaccess")
 
 #elif defined(__clang__) // unix + gcc/clang
 
@@ -40,6 +42,7 @@
 #define DISABLE_WARNING_DEPRECATED_DECLARATIONS DISABLE_WARNING("-Wdeprecated-declarations")
 #define DISABLE_WARNING_UNUSED_VALUE DISABLE_WARNING("-Wunused-value")
 #define DISABLE_WARNING_MAYBE_UNINITIALIZED // not supported by clang
+#define DISABLE_WARNING_CLASS_MEMACCESS DISABLE_WARNING("-Wclass-memaccess")
 
 #elif defined(__GNUC__) // unix + gcc/clang
 #define DO_PRAGMA(X) _Pragma(#X)
@@ -52,13 +55,14 @@
 #define DISABLE_WARNING_DEPRECATED_DECLARATIONS DISABLE_WARNING("-Wdeprecated-declarations")
 #define DISABLE_WARNING_UNUSED_VALUE DISABLE_WARNING("-Wunused-value")
 #define DISABLE_WARNING_MAYBE_UNINITIALIZED DISABLE_WARNING("-Wmaybe-uninitialized")
-
+#define DISABLE_WARNING_CLASS_MEMACCESS DISABLE_WARNING("-Wclass-memaccess")
 #else
 #define DISABLE_WARNING_PUSH
 #define DISABLE_WARNING_POP
 #define DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER
 #define DISABLE_WARNING_UNUSED_VALUE
 #define DISABLE_WARNING_UNREFERENCED_FUNCTION
+#define DISABLE_WARNING_CLASS_MEMACCESS
 // other warnings you want to deactivate...
 
 #endif
